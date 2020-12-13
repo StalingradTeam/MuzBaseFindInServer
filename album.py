df1ab3f0e3587cb3fe1115819e9c85bc007639e1
©StalingradTeam <70740073+StalingradTeam@users.noreply.github.com>
2020-12-13 16:36:06 +0400

    Add files via upload

---
+++ album.py
@@ -0,0 +1,66 @@
+import sqlalchemy as sa
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+DB_PATH = "sqlite:///albums.sqlite3"
+Base = declarative_base()
+
+
+class Error(Exception):
+    pass
+
+
+class AlreadyExists(Error):
+    pass
+
+
+class Album(Base):
+
+    __tablename__ = "album"
+
+    id = sa.Column(sa.INTEGER, primary_key=True)
+    year = sa.Column(sa.INTEGER)
+    artist = sa.Column(sa.TEXT)
+    genre = sa.Column(sa.TEXT)
+    album = sa.Column(sa.TEXT)
+
+
+def connect_db():
+
+    engine = sa.create_engine(DB_PATH)
+    Base.metadata.create_all(engine)
+    session = sessionmaker(engine)
+    return session()
+
+
+def find(artist):
+
+    session = connect_db()
+    albums = session.query(Album).filter(Album.artist == artist).all()
+    return albums
+
+
+def save(year, artist, genre, album):
+    if not (isinstance(year, int)):
+        raise AssertionError
+    elif not isinstance(genre, str):
+        raise AssertionError
+    elif not isinstance(genre, str):
+        raise AssertionError
+    elif not isinstance(album, str):
+        raise AssertionError
+    else:
+        session = connect_db()
+        saved_album = session.query(Album).filter(Album.album == album, Album.artist == artist).first()
+        if saved_album is not None:
+            raise AlreadyExists("Album already exists and has #{}".format(saved_album.id))
+
+        album = Album(
+            year=year,
+            artist=artist,
+            genre=genre,
+            album=album
+        )
+        session.add(album)
+        session.commit()
+    return album
